57e15a000f3000139bd69b21eecb01f4e3540b49
Arbieu Lucien <68686033+Lucien34000@users.noreply.github.com>
2020-10-08 15:48:32 +0200

    Add files via upload

--- js/panier.js
+++ js/panier.js
@@ -119,9 +119,9 @@ function validRegex() {
                     localStorage.removeItem('itemsInCart');
                     document.location = 'order.html?order=' + data.orderId;
                 });
-            })
-        }
-    }
+            });
+        };
+    };
 
 function formV(Regtest, input) {
     if (Regtest) {
@@ -147,8 +147,7 @@ function cartNum() {
         localStorage.setItem('productNumbers', 1);
         document.querySelector('#cart').textContent = 1;
     }
-}
-
+};
 
 // Fonction qui supprime un produit que ce soit sur la nav bar, bouton supprimer(produit), gérer les quantitées
 function cartNumDeleted() {
@@ -162,23 +161,29 @@ function cartNumDeleted() {
         localStorage.setItem('productNumbers', 1);
         document.querySelector('#cart').textContent = 1;
     }
-}
+};
 
 // Fonction pour supprimer un produit du panier
-function deleteProduct(id) {  
-    document.getElementById("deleteButton-" + id).addEventListener("click", function ()  {       
-       let productsIds = localStorage.getItem("itemsInCart");   
-       productsIds = JSON.parse(productsIds); 
+const deleteRowButtons = document.getElementsByClassName("delete-row");
+let productID = JSON.parse(localStorage.getItem("itemsInCart"));
 
-       if (productsIds !== null) {
-           productsIds.remove();
-           localStorage.setItem("itemsInCart", productsIds);  
-           location.reload(); 
-       }   
-   });
-}
+for (let i = 0; i < deleteRowButtons.length; i++) {
+    deleteRowButtons[i].addEventListener('click', (e) => {
 
+       for (let x = 0; x < productID.length; x++) {
 
+            if (productID[productID.key(x)] == deleteRowButtons[i].getAttribute("data-id")) {
+
+                localStorage.delete(productID[productID.key(x)]);
+                localStorage.setItem('itemsInCart', productID[productID.key(x)]);
+                window.location.reload(true);
+                console.log(productID);
+            };
+       }; 
+    console.log(productID.key(x));
+    console.log(deleteRowButtons[i].getAttribute("data-id"));
+    });
+};
 
 // Fonction pour faire le total de produits dans le panier
 function total() {
@@ -225,7 +230,7 @@ function displayCart() {
                             </div>
                         </div>
                         <div class = "col-4 col-sm-2 col-md-2 text-right">
-                            <button id="deleteButton-${item[0].id}" type="button" class="btn btn-outline-danger btn-xs">
+                            <button id="${item[0].id}" data-id="${item[0].id}" type="button" class="btn btn-outline-danger btn-xs delete-row">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
